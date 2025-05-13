cde425379fe912da9f6968b7754f490a7568103c
sgoriashin <serega.goryashin@gmail.com>
2025-05-14 01:21:38 +0400

    Added Docker with PostgreSQL support

---
+++ start-db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Выполняется запуск Docker-контейнера с PostgreSQL .."
+docker-compose up -d
+echo "Docker-контейнер с PostgreSQL запущен"
---
+++ stop-db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Выполняется остановка Docker-контейнера с PostgreSQL..."
+docker-compose down
+echo "Docker-контейнер с PostgreSQL остановлен"
