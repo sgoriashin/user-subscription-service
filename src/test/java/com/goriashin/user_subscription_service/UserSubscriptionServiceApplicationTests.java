1239c7d2dbd47864a9f496feac8a2ab2a4faa0bd
sgoriashin <serega.goryashin@gmail.com>
2025-05-13 20:59:08 +0400

    Initial commit

---
+++ src/test/java/com/goriashin/user_subscription_service/UserSubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goriashin.user_subscription_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSubscriptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
